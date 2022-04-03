e6d7f445868a0d3a33c873eb536821e8efd72071
wilfriedf <wilfriedfouillaret@gmail.com>
2022-04-03 10:39:17 +0200

    add new params and interaction constraints

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from train import *
 if __name__ == "__main__":
     create_figure()
     X_train, X_test, y_train, y_test = prepare_data()
-    bst = training(X_train, X_test, y_train, y_test)
-    preds = get_preds(bst)
+    best_model = training(X_train, X_test, y_train, y_test)
+    preds = get_preds(best_model)
     make_submission(preds)
--- train.py
+++ train.py
@@ -7,55 +7,48 @@ import numpy as np
 import pandas as pd
 import xgboost as xgb
 
+from sklearn.model_selection import GridSearchCV
+
 warnings.filterwarnings("ignore")
 
 MODELS_DIR = Path.cwd().parent / "_pawsitive" / "models"
 DATA_DIR = Path.cwd().parent / "_pawsitive" / "data"
-SUBMISSION_DIR = Path.cwd().parent / "_pawsitive" /  "data" / "submissions"
+SUBMISSION_DIR = Path.cwd().parent / "_pawsitive" / "data" / "submissions"
 
 
 def training(X_train, X_test, y_train, y_test):
-
     # store the data ib DMatrix
     d_train = xgb.DMatrix(X_train, label=y_train)
     d_test = xgb.DMatrix(X_test, label=y_test)
 
     # set params
-    hyperparameters = {
-        "max_depth": 5,
-        "eta": 0.1,
-        "tree_method": "hist",
-        "objective": "reg:squarederror",
-        "eval_metric": "rmse",
-        "nthread": 4,
-        "seed": 1302,
-        "gamma": 0.01
-    }
+    params = dict(max_depth=5, eta=0.1, tree_method="exact", objective="reg:squarederror", eval_metric="rmse",
+                  nthread=4, seed=1302, gamma=0.04, interaction_constraints="[[0, 1], [0, 2], [0, 3]]", reg_lambda=0.4,
+                  reg_alpha=0.1)
+
     # start training monitoring scores
     watchlist = [(d_test, "eval"), (d_train, "train")]
-    bst = xgb.train(hyperparameters, d_train, 10, watchlist)
+    bst = xgb.train(params, d_train, 15, watchlist)
 
     # use predictions to improve the model
     ptrain = bst.predict(d_train, output_margin=True)
     ptest = bst.predict(d_test, output_margin=True)
     d_train.set_base_margin(ptrain)
     d_test.set_base_margin(ptest)
-    bst_with_predictions = xgb.train(hyperparameters, d_train, 10, watchlist)
+    bst_with_predictions = xgb.train(params, d_train, 15, watchlist)
 
     # save the model
-    bst_with_predictions.save_model(MODELS_DIR / '002.model')
+    bst_with_predictions.save_model(MODELS_DIR / '003.model')
 
     return bst_with_predictions
 
 
 def get_preds(model):
-
     # load and prepare testing set
     df = pd.read_csv(DATA_DIR / "test.csv", index_col="id", header=0)
     df = pd.get_dummies(df, drop_first=True)
     df = df.astype(np.float32)
     d_test = xgb.DMatrix(df)
-
     predictions = model.predict(d_test, iteration_range=(0, model.best_iteration))
 
     return predictions
@@ -64,5 +57,5 @@ def get_preds(model):
 def make_submission(predictions):
     df = pd.read_csv(DATA_DIR / "submission_format.csv", index_col="id", header=0)
     df["lines_per_sec"] = predictions
-    df.to_csv(SUBMISSION_DIR / "submission002.csv")
+    df.to_csv(SUBMISSION_DIR / "submission003.csv")
     print(df.head(10))
